fb629b3917a2f50f5de460286e889af89d8326c1
m0in92 <moinahmed100@gmail.com>
2023-05-08 20:14:35 -0400

    May 8, 2023 - last commit

--- src/model/thermal.py
+++ src/model/thermal.py
@@ -4,14 +4,14 @@ from src.model.ecm import Thevenin1RC
 class Lumped:
     def __init__(self, ecm_obj):
         if not isinstance(ecm_obj, Thevenin1RC):
-            raise TypeError("ecm_obj needs to be a Thevenin 1 RC object.")
+            raise TypeError("ecm_obj needs to be a Thevenin1RC object.")
         self.ecm_obj = ecm_obj
 
     def reversible_heat(self, I, T):
         return I * T * (self.b_cell.elec_p.dOCPdT - self.b_cell.elec_n.dOCPdT)
 
-    def irreversible_heat(self, I, V):
-        return I * (V - (self.b_cell.elec_p.OCP - self.b_cell.elec_n.OCP))
+    def irreversible_heat(self, I, V, soc):
+        return I * (V - self.ecm_obj.OCV_func(soc))
 
     def heat_flux(self, T):
         return self.b_cell.h * self.b_cell.A * (T - self.b_cell.T_amb)
